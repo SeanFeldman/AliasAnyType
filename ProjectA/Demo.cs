8fcaef56e43d2c9580d2d7993386517827baa0e5
Sean Feldman <feldman.sean@gmail.com>
2024-11-16 21:46:40 -0700

    repro

---
+++ ProjectA/Demo.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectA;
+
+public class Demo
+{
+    public Demo()
+    {
+        NamedTuple nt;
+        NamedTupleList ntl;
+
+        // NamedTupleWithNullables ntwn;
+        // NamedTupleWithNullablesList ntwnl;
+    }
+}
---
+++ ProjectA/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+// global using NamedTuple = (bool isUsed, string value);
+// global using NamedTupleList = System.Collections.Generic.List<(int one, int two)>;
+//
+// global using NamedTupleWithNullables = (bool isUsed, string key, string? value);
+// global using NamedTupleWithNullablesList = System.Collections.Generic.List<(bool isUsed, string key, string? value)>;
